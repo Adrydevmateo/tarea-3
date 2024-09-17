a826a00c702b99a8a2c7c37a118de427f7962382
AdryDev <adrydevmateo@gmail.com>
2024-09-17 08:10:40 -0400

    Working on sum page

--- src/pages/sum/Sum.tsx
+++ src/pages/sum/Sum.tsx
@@ -8,10 +8,26 @@ import {
 	IonToolbar,
 } from "@ionic/react";
 import "./Sum.css";
+import type { FormEvent } from "react";
 
 // TODO: Una página donde se puedan sumar dos números y mostrar el resultado.
 
 export default function Sum() {
+	function handleSubmit(e: FormEvent) {
+		e.preventDefault();
+		const form = e.target as HTMLFormElement;
+		const formData = new FormData(form);
+
+		const x = Number(formData.get("x"));
+		if (!x) alert("Por favor introduce el primer número");
+
+		const y = Number(formData.get("y"));
+		if (!y) alert("Por favor introduce el primer número");
+
+		const result = document.getElementById("resultado") as HTMLElement;
+		result.innerHTML = String(x + y);
+	}
+
 	return (
 		<IonPage>
 			<IonHeader>
@@ -29,18 +45,22 @@ export default function Sum() {
 						<IonTitle size="large">Sumadora</IonTitle>
 					</IonToolbar>
 				</IonHeader>
-				<div className="page_container">
-					<strong>qwe</strong>
-					<p>
-						Explore{" "}
-						<a
-							target="_blank"
-							rel="noopener noreferrer"
-							href="https://ionicframework.com/docs/components"
-						>
-							UI Components
-						</a>
-					</p>
+				<div className="page_container" id="sum-page">
+					<form onSubmit={handleSubmit}>
+						<div className="form-field">
+							<label htmlFor="x">Ingresa el primer número</label>
+							<input type="number" name="x" id="x" />
+						</div>
+
+						<div className="form-field">
+							<label htmlFor="y">Ingresa el segundo número</label>
+							<input type="number" name="y" id="y" />
+						</div>
+
+						<button type="submit">Sumar</button>
+					</form>
+					<hr />
+					<p id="resultado">Resultado: {}</p>
 				</div>
 			</IonContent>
 		</IonPage>
