f6fd1183ad5986ce43c0bb7ad3f01ffc97de9a05
AdryDev <adrydevmateo@gmail.com>
2024-09-17 19:13:07 -0400

    Added mul page logic

--- src/pages/mul/Mul.tsx
+++ src/pages/mul/Mul.tsx
@@ -8,10 +8,15 @@ import {
 	IonToolbar,
 } from "@ionic/react";
 import "./Mul.css";
+import { useState, type FormEvent } from "react";
 
 // TODO: Un programa que acepte un número y muestre su tabla de multiplicar hasta el 13.
 
 export default function Mul() {
+	const [x, setX] = useState(0);
+
+	const mul = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
 	return (
 		<IonPage>
 			<IonHeader>
@@ -29,18 +34,33 @@ export default function Mul() {
 						<IonTitle size="large">Tabla de Multiplicar</IonTitle>
 					</IonToolbar>
 				</IonHeader>
-				<div className="page_container">
-					<strong>qwe</strong>
-					<p>
-						Explore{" "}
-						<a
-							target="_blank"
-							rel="noopener noreferrer"
-							href="https://ionicframework.com/docs/components"
-						>
-							UI Components
-						</a>
-					</p>
+				<div className="page_container" id="mul-page">
+					<form>
+						<label htmlFor="x">Ingresa un número:</label>
+						<input
+							type="number"
+							name="x"
+							id="x"
+							value={x}
+							onChange={({ target }) => setX(Number(target.value))}
+						/>
+					</form>
+
+					<hr />
+
+					<div>
+						<ol>
+							{mul.map((m) => (
+								<li key={m}>
+									<b>{m}</b>
+									<b>x</b>
+									<b>{x}</b>
+									<b> = </b>
+									<b>{m * x}</b>
+								</li>
+							))}
+						</ol>
+					</div>
 				</div>
 			</IonContent>
 		</IonPage>
