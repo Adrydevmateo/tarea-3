45f5e6c92b8464288af0aa0266ecc47ab2dceb48
AdryDev <adrydevmateo@gmail.com>
2024-09-19 19:00:47 -0400

    Fixed translator

--- src/pages/Translator.tsx
+++ src/pages/Translator.tsx
@@ -12,44 +12,95 @@ import {
 } from "@ionic/react";
 import { useState } from "react";
 
-export default function Translator() {
-	const [x, setX] = useState(0);
-	const letterList = [
-		"a",
-		"b",
-		"c",
-		"d",
-		"e",
-		"f",
-		"g",
-		"h",
-		"i",
-		"j",
-		"k",
-		"l",
-		"m",
-		"n",
-		"ñ",
-		"o",
-		"p",
-		"q",
-		"r",
-		"s",
-		"t",
-		"u",
-		"v",
-		"w",
-		"x",
-		"y",
-		"z",
+function translate(number: number): string {
+	const units = [
+		"cero",
+		"uno",
+		"dos",
+		"tres",
+		"cuatro",
+		"cinco",
+		"seis",
+		"siete",
+		"ocho",
+		"nueve",
 	];
 
-	const updateX = (newValue: string) => {
-		const parsedValue = Number(newValue);
-		const max = letterList.length - 1;
-		if (parsedValue > max) return;
-		setX(parsedValue);
-	};
+	const tens = [
+		"",
+		"diez",
+		"veinte",
+		"treinta",
+		"cuarenta",
+		"cincuenta",
+		"sesenta",
+		"setenta",
+		"ochenta",
+		"noventa",
+	];
+
+	const hundreds = [
+		"",
+		"ciento",
+		"doscientos",
+		"trescientos",
+		"cuatrocientos",
+
+		"quinientos",
+		"seiscientos",
+		"setecientos",
+		"ochocientos",
+		"novecientos",
+	];
+
+	if (number === 0) {
+		return "cero";
+	}
+
+	if (number < 10) {
+		return units[number];
+	}
+
+	if (number < 20) {
+		const x = `dieci${units[number % 10]}`;
+		if (x === "diecicero") return "diez";
+		if (x === "dieciuno") return "once";
+		if (x === "diecidos") return "doce";
+		if (x === "diecitres") return "trece";
+		if (x === "diecicuatro") return "catorce";
+		if (x === "diecicinco") return "quince";
+		return `dieci${units[number % 10]}`;
+	}
+
+	if (number < 100) {
+		const x =
+			tens[Math.floor(number / 10)] +
+			(number % 10 !== 0 ? ` y ${units[number % 10]}` : "");
+
+		if (x === "veinte y uno") return "veintiuno";
+		if (x === "veinte y dos") return "veintidós";
+		if (x === "veinte y tres") return "veintitrés";
+		if (x === "veinte y cuatro") return "veinticuatro";
+		if (x === "veinte y cinco") return "veinticinco";
+		if (x === "veinte y seis") return "veintiséis";
+		if (x === "veinte y siete") return "veintisiete";
+		if (x === "veinte y ocho") return "veintiocho";
+		if (x === "veinte y nueve") return "veintinueve";
+		return x;
+	}
+
+	if (number < 1000) {
+		return (
+			hundreds[Math.floor(number / 100)] +
+			(number % 100 !== 0 ? ` ${translate(number % 100)}` : "")
+		);
+	}
+
+	return "mil";
+}
+
+export default function Translator() {
+	const [x, setX] = useState(0);
 
 	return (
 		<IonPage>
@@ -69,18 +120,17 @@ export default function Translator() {
 							label="Ingresa un número:"
 							placeholder="Ejemplo: 2"
 							type="number"
-							max={letterList.length - 1}
+							max={1000}
+							min={0}
 							value={x}
 							onInput={({ target }) =>
-								updateX((target as HTMLInputElement).value)
+								setX(Number((target as HTMLInputElement).value))
 							}
 						/>
 					</IonItem>
 					<hr />
 					<IonItem>
-						<b style={{ textTransform: "capitalize" }}>
-							Letra: {letterList[x]}
-						</b>
+						<b>Traducido a: {translate(x)}</b>
 					</IonItem>
 				</IonList>
 			</IonContent>
